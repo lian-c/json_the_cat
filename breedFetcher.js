a52788a11bf68cdd6ff9417a8e4e3300562482bf
lian-c <lian.chan@telus.com>
2022-11-24 18:10:02 -0800

    Created a request to grab content from the API of thecatapi , converted from string to object with JSON.parse

---
+++ breedFetcher.js
@@ -0,0 +1,12 @@
+const request = require('request');
+const URL = 'https://api.thecatapi.com/v1/breeds/search?q='
+
+
+request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, content) => {
+  
+if(error){
+console.log('error:', error);} // Print the error if one occurred
+  const data = JSON.parse(content.body)
+  console.log(data); 
+  console.log('Typeof: ',typeof data); 
+});
