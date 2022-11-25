b349a4f81ac6208f9a60e70f8bf72950041764fb
lian-c <lian.chan@telus.com>
2022-11-24 18:37:57 -0800

    forgot to eslint, eslint the file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,22 @@
 const request = require('request');
 const args = process.argv.slice(2); //skips the first two
-const breed = args[0]
+const breed = args[0];
 
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, content) => {
   
-  if(error){
+  if (error) {
     console.log('Error❗️', error);
-    console.log("Please check the URL and try again.")
+    console.log("Please check the URL and try again.");
     return;
   } // Print the error if one occurred
-  if (content.body === '[]'){ //data didn't work so pre parse it works
-    console.log("Error❗️ Not a valid breed 🐱. Please check the breed name and try again.")
+  if (content.body === '[]') { //data didn't work so pre parse it works
+    console.log("Error❗️ Not a valid breed 🐱. Please check the breed name and try again.");
     return;
   }
   
-  const data = JSON.parse(content.body)
+  const data = JSON.parse(content.body);
   console.log(data[0]["description"]); //grabs only the description
-  // console.log('Typeof: ',typeof data); 
+  // console.log('Typeof: ',typeof data);
 
 });
