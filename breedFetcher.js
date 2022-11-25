d25fc49ac6313cd23c1b53532a9f23e47578fc6a
lian-c <lian.chan@telus.com>
2022-11-24 20:25:02 -0800

    added test with mocha/chai and made sure code still worked

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,14 +4,14 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, res, body) => {
     if (error) {
-      return callback("Please check the url.", null);
+      return callback("error", null);
     }
     const data = JSON.parse(body);
     const dataObj = data[0];
     // const description = dataObj["description"] cannot read property
 
     if (dataObj === undefined) {
-      return callback("This breed does not exist, please check again", null);
+      return callback("Invalid breed", null);
     } else {
       return callback(null, dataObj["description"]);
     }
