fbcaf61a5e44c8599dac1c87648c0c0291709a02
lian-c <lian.chan@telus.com>
2022-11-24 18:32:39 -0800

    Added edge case for error or wrong breed and tested a few times to ensure  code is working . added some emoji for fun

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,22 @@
 const request = require('request');
-const URL = 'https://api.thecatapi.com/v1/breeds/search?q='
+const args = process.argv.slice(2); //skips the first two
+const breed = args[0]
 
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, content) => {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, content) => {
   
-if(error){
-console.log('error:', error);} // Print the error if one occurred
+  if(error){
+    console.log('Error❗️', error);
+    console.log("Please check the URL and try again.")
+    return;
+  } // Print the error if one occurred
+  if (content.body === '[]'){ //data didn't work so pre parse it works
+    console.log("Error❗️ Not a valid breed 🐱. Please check the breed name and try again.")
+    return;
+  }
+  
   const data = JSON.parse(content.body)
-  console.log(data[0]); 
-  console.log('Typeof: ',typeof data); 
+  console.log(data[0]["description"]); //grabs only the description
+  // console.log('Typeof: ',typeof data); 
+
 });
