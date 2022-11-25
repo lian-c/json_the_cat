7265ce98d26f2486a45452ecfb290ee490d59c10
lian-c <lian.chan@telus.com>
2022-11-24 19:44:26 -0800

    having issue with error showing up when there's no error so debugging and realized i forgot the response in request which is why body.body worked before, tried to look for error status in but wasn't able to find it will try another method

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,28 @@
 const request = require('request');
+const breedName = process.argv[2];
 
 const fetchBreedDescription = function(breedName, callback){
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, content) => {
-    const data = JSON.parse(content.body);
-    const result = data[0]["description"];
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error,res, body) => { 
+
+    const data = JSON.parse(body);
+    // const result = data[0]["description"];
     
-    callback(result)
-    return result;
-})
+    if (error){
+      return error;
+    }
+    
+    return callback(console.log(data))
+    
+  })
 };
+
+fetchBreedDescription(breedName, (error, desc) => {
+console.log(desc)
+  // if (error) {
+  //   console.log('Error fetch details:', error);
+  // } else {
+  //   console.log(desc);
+  // }
+});
 
 module.exports = { fetchBreedDescription };
