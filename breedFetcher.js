d01d4eec0cb0eec385fa7a6e72b51df3fdf3b24b
lian-c <lian.chan@telus.com>
2022-11-24 20:03:15 -0800

    realized callback needed 2 parameters so the callback assumed the information was treated as an error not the desc

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,21 @@
 const request = require('request');
 
 
-const fetchBreedDescription = function(breedName, callback){
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error,res, body) => { 
-
+const fetchBreedDescription = function (breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, res, body) => {
+    if (error) {
+      return callback("Please check the url.", null)
+    }
     const data = JSON.parse(body);
     const dataObj = data[0];
-    const description = dataObj["description"]
-    
-    if (dataObj === undefined){
-      return callback(dataObj);
+    // const description = dataObj["description"] cannot read property
+
+    if (dataObj === undefined) {
+      return callback("This breed does not exist, please check again", null);
     }
-    
-   callback(description)
-    
+    else {
+      return callback(null, dataObj["description"])
+    }
   })
 };
 
