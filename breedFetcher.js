82b7573615e390d8d8f484ed2911c1bde3643421
lian-c <lian.chan@telus.com>
2022-11-24 18:11:19 -0800

    taken the first data array and consolelog

--- breedFetcher.js
+++ breedFetcher.js
@@ -7,6 +7,6 @@ request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, content) => 
 if(error){
 console.log('error:', error);} // Print the error if one occurred
   const data = JSON.parse(content.body)
-  console.log(data); 
+  console.log(data[0]); 
   console.log('Typeof: ',typeof data); 
 });
